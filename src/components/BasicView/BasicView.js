1b73f8a8eadcc0c16967987a5ab55e4d372f5b11
Egor Geller <egkingers11@gmail.com>
2022-09-24 02:43:32 +0300

    Delete form tag

--- src/components/BasicView/BasicView.js
+++ src/components/BasicView/BasicView.js
@@ -9,46 +9,44 @@ import MainForm from "../MainForm/MainForm";
 import FirstTable from "../FirstTable/FirstTable";
 
 const BasicView = () => (
-  <Container dir="rtl" fluid="md">
-    <Row>
-      <Col sm={8}>
-        <h2>Fly Electric LTD.</h2>
-        <p>ח.פ. 515337251</p>
-      </Col>
-      <Col sm={4}>
-        <Figure>
-          <Figure.Image
-            width={100}
-            height={100}
-            alt="FlyTag logo"
-            src="images/FlyTagLogo.jpeg"
-          />
-        </Figure>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <Form>
-          <Row md="auto">
-            <MainForm />
-          </Row>
-          <Container>
-            <Form.Group as={Row} className="mb-3" controlId="invoiceNumber">
-              <Form.Label column sm={2}>
-                הצעת מחיר
-              </Form.Label>
-              <Col>
-                <Form.Control type="text" placeholder="" />
-              </Col>
-            </Form.Group>
-          </Container>
-          <Row>
-            <FirstTable />
-          </Row>
-        </Form>
-      </Col>
-    </Row>
-  </Container>
+    <Container dir="rtl" fluid="md">
+      <Row>
+        <Col sm={8}>
+          <h2>Fly Electric LTD.</h2>
+          <p>ח.פ. 515337251</p>
+        </Col>
+        <Col sm={4}>
+          <Figure>
+            <Figure.Image
+              width={100}
+              height={100}
+              alt="FlyTag logo"
+              src="images/FlyTagLogo.jpeg"
+            />
+          </Figure>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+            <Row md="auto">
+              <MainForm />
+            </Row>
+            <Container>
+              <Form.Group as={Row} className="mb-3" controlId="invoiceNumber">
+                <Form.Label column sm={2}>
+                  הצעת מחיר
+                </Form.Label>
+                <Col>
+                  <Form.Control type="text" placeholder="" />
+                </Col>
+              </Form.Group>
+            </Container>
+            <Row>
+              <FirstTable />
+            </Row>
+        </Col>
+      </Row>
+    </Container>
 );
 
 BasicView.propTypes = {};
