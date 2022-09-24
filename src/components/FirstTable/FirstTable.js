31cafe0ca94e898f1f971dff4ca45e7e4f5ed53c
Egor Geller <egkingers11@gmail.com>
2022-09-24 17:40:48 +0300

    implement bootstrap table next

--- src/components/FirstTable/FirstTable.js
+++ src/components/FirstTable/FirstTable.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
+import BootstrapTable from "react-bootstrap-table-next";
+import CellEditFactory from "react-bootstrap-table2-editor";
 import AddItem from "../AddItem/AddItem";
 import { read, utils } from "xlsx";
 
@@ -36,19 +38,58 @@ const FirstTable = () => {
   }, []);
 
   const getAddedItem = useCallback(async function () {
-      const d = localStorage.getItem("addItem");
-      const newAddItem = JSON.parse(d);
+    const d = localStorage.getItem("addItem");
+    const newAddItem = JSON.parse(d);
 
-      if(d) {
-        setData(newAddItem);
-      }
-        
-    },[]);
+    if (d) {
+      setData(newAddItem);
+    }
+  }, []);
+
+  console.log(data);
 
   useEffect(() => {
     getJsonData();
     getAddedItem();
   }, [getJsonData, getAddedItem]);
+
+  const columns = [
+    {
+      //dataField: "id",
+      text: "#",
+      formatter: (cell, row, rowIndex, extraData) => (
+        <div>
+          <span>ID: {row.id}</span>
+          <br />
+          <span>state: {extraData}</span>
+        </div>
+      )
+    },
+    {
+      dataField: "item",
+      text: "תאור מוצר",
+      editable: false,
+    },
+    {
+      dataField: "measure",
+      text: "יחידת מידה",
+      editable: false,
+    },
+    {
+      dataField: "amount",
+      text: "כמות",
+    },
+    {
+      dataField: "priceForOne",
+      text: "מחיר יח׳",
+      editable: false,
+    },
+    {
+      dataField: "total",
+      text: "מחיר מלא",
+      editable: false,
+    },
+  ];
 
   return (
     <div>
@@ -70,7 +111,34 @@ const FirstTable = () => {
       <br></br>
       <AddItem items={items} />
       <br></br>
-      <Table striped bordered hover>
+      <BootstrapTable
+        keyField="id"
+        data={data}
+        columns={columns}
+        striped
+        hover
+        condensed
+        noDataIndication="Table is Empty"
+        cellEdit={CellEditFactory({
+          mode: "click",
+          blurToSave: true,
+          afterSaveCell: (oldValue, newValue, row, column) => {
+            data.map((d, idx) => {
+              if (d.id === row.id) {
+                const d = localStorage.getItem("addItem");
+                const newAddItem = JSON.parse(d);
+                newAddItem[idx].amount = newValue;
+                newAddItem[idx].total = newValue * newAddItem[idx].priceForOne;
+                localStorage.setItem("addItem", JSON.stringify(newAddItem));
+                window.location.reload();
+              }
+              return 1;
+            });
+          },
+          autoSelectText: true,
+        })}
+      />
+      {/* <Table striped bordered hover>
         <thead>
           <tr>
             <th>#</th>
@@ -92,13 +160,12 @@ const FirstTable = () => {
                 <td>{d.priceForOne}</td>
                 <td>{d.total}</td>
               </tr>
-            )
-          )
-            ) : (
-              <></>
-            )}
+            ))
+          ) : (
+            <></>
+          )}
         </tbody>
-      </Table>
+      </Table> */}
     </div>
   );
 };
