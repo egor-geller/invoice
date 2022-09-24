25cd7b5f910376f438e2ea505c8646448188bfc5
Egor Geller <egkingers11@gmail.com>
2022-09-25 01:34:12 +0300

    Add component Sum and info

--- src/components/BasicView/BasicView.js
+++ src/components/BasicView/BasicView.js
@@ -7,12 +7,20 @@ import Figure from "react-bootstrap/Figure";
 import Form from "react-bootstrap/Form";
 import MainForm from "../MainForm/MainForm";
 import FirstTable from "../FirstTable/FirstTable";
+import Sum from "../Sum/Sum";
+import Footer from "../Footer/Footer";
+import Button from "react-bootstrap/esm/Button";
 
-const BasicView = () => (
-    <Container dir="rtl" fluid="md">
+const BasicView = () => {
+  return (
+    <Container
+      dir="rtl"
+      fluid="md"
+      style={{ marginTop: "10px", marginBottom: "20px" }}
+    >
       <Row>
         <Col sm={8}>
-          <h2>Fly Electric LTD.</h2>
+          <h2>פלאיי אלקטריק בע״מ</h2>
           <p>ח.פ. 515337251</p>
         </Col>
         <Col sm={4}>
@@ -28,26 +36,55 @@ const BasicView = () => (
       </Row>
       <Row>
         <Col>
-            <Row md="auto">
-              <MainForm />
-            </Row>
-            <Container>
-              <Form.Group as={Row} className="mb-3" controlId="invoiceNumber">
-                <Form.Label column sm={2}>
-                  הצעת מחיר
-                </Form.Label>
-                <Col>
-                  <Form.Control type="text" placeholder="" />
-                </Col>
-              </Form.Group>
-            </Container>
-            <Row>
-              <FirstTable />
-            </Row>
+          <Row>
+            <MainForm />
+          </Row>
+          <Row>
+            <Form.Group as={Row} className="mb-3" controlId="invoiceNumber">
+              <Form.Label column sm={2}>
+                הצעת מחיר
+              </Form.Label>
+              <Col>
+                <Form.Control type="text" placeholder="" />
+              </Col>
+              <Button as={Col} variant="success" style={{maxWidth: "10%"}}>
+                Export
+              </Button>
+            </Form.Group>
+          </Row>
+          <Row>
+            <FirstTable />
+          </Row>
+          <Row>
+            <Col dir="rtl">
+              <p style={{ display: "grid", textAlign: "right" }}>
+                המחירים אינם כוללים מע"מ.
+                <br />
+                <b>מחיר סופי נקבע על פי כמויות לאחר ביצוע בפעול.</b>
+                <br />
+                ההצעה מתייחסת לביצוע העבודה ברצף ללא הפסקות או הפרעות.
+                <br />
+                תוקף ההצעה 30 ימים.
+                <br />
+                <b>המחיר לתשלום :שוטף 30+ יום.</b>
+                <br />
+                <b>אשמח לעמוד לרשותך לכל שאלה או בקשה.</b>
+                <br />
+                <b>איליה גלר</b>
+              </p>
+            </Col>
+            <Col style={{ alignSelf: "center" }}>
+              <Sum />
+            </Col>
+          </Row>
+          <Row>
+            <Footer />
+          </Row>
         </Col>
       </Row>
     </Container>
-);
+  );
+};
 
 BasicView.propTypes = {};
 
